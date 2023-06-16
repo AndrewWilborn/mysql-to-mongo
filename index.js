6411df256a2e10c190806d4e14255e426e4bd284
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-06-16 14:44:31 -0400

    ADDED comments

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ import mysql from "mysql2/promise";
 import { mongoURI } from "./credentials.js";
 import { sqlLogin } from "./credentials.js";
 
+// Connect to SQL Database
 const db = await mysql.createConnection(sqlLogin);
+// Read data from database and store it in movieList
 const [movieList] = await db.execute("SELECT * FROM movies")
+// Close SQL connection
 db.end();
 
-console.log(movieList)
-
+// Connect to mongo database
 const mongoConnection = new MongoClient(mongoURI);
 await mongoConnection.connect();
+// Select the c11-practice collection
 const mongoDatabase = mongoConnection.db("c11-practice");
 
+// Insert the data in movieList array into the collection
 await mongoDatabase.collection("movies").insertMany(movieList);
 
+// Close mongo connection
 mongoConnection.close();
